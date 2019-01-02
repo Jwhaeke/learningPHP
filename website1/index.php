034ac532bd9c0242800ad3c37b0f5506f83e0639
jwhaeke <jwhaeke@live.nl>
2019-01-02 13:53:40 +0100

    worked on require and include

---
+++ website1/index.php
@@ -0,0 +1,8 @@
+<!--    include allows for script to continue even if file is not found.
+        require will force a fatal error
+-->
+
+<?php include 'inc/header.php'; ?>
+    <h1>Home</h1>
+<?php include 'inc/footer.php'; ?>
+
